12c1bdced2ccc4d18a489715d5e1f0f3d575b987
Mrutsy <bins@zhilk.in>
2022-05-14 18:48:59 +0700

    14.05.2022 v2 1. Добавлен отслеживатель запросов к сайту. Определяет как обращаются к сайту, по методам POST или REQUEST или же обычно заходят. 2. Добавил обращение по методу API и Route (маршрутизация по сайту). 3. Очищен старый закомментированный код.

--- index.php
+++ index.php
@@ -72,197 +72,65 @@ class system
             $this->attach_files($path, $type);
         }
     }
+
+    public function route ($path)
+    {
+        echo "ok";
+    }
+}
+
+
+$sys = new system();
+
+
+
+if ($_POST || $_REQUEST) {
+
+    if ($_POST) {
+        $request = $_POST;
+    } else {
+        $request = $_REQUEST;
+    }
+
+    if ($request['method'] == "api") {
+        echo "API - ok";
+        echo $request['value'];
+    } elseif ($request['method'] == "route") {
+        echo "Route - ok";
+        echo $request['value'];
+    } else {
+        echo "Неизвестный метод запроса.";
+    }
+
+    //echo "Подключение запросом: ".var_dump($request);
+
+} else {
+    echo "Обычное подключение.<br>";
+    $sys->attach("v/stable/core.php");
 }
 
 
 
 
-$sys = new system();
-$sys->attach("v/stable/core.php");
 
+?>
 
+<html>
+<head>
+    <title>DEADBEE - DEV.</title>
+</head>
+<body>
 
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-///**
-// * @property $path
-// * @property $type
-// */
-//class system
-//{
-//
-//
-//
-//    public function __construct()
-//    {
-//    }
-//
-//
-//
-//
-//
-//    private function attach_file($path)
-//    {
-//        require_once $path;
-//
-//    }
-//
-//
-//
-//
-//    private function attach_files ($path, $type)
-//    {
-//
-//        foreach (scandir($path, 1) as $chunk) {
-//
-//            if (is_file($path."/".$chunk)) {
-//
-//
-//                $expansion = new SplFileInfo($chunk);
-//
-//                if ($type == "all") {
-//
-//                    $this->attach_file($path."/".$chunk);
-//
-//                } else {
-//
-//                    if ($expansion->getExtension() == $type) {
-//
-//                        $this->attach_file($path."/".$chunk);
-//
-//                    }
-//
-//                }
-//
-//            } elseif (is_dir($path."/".$chunk)) {
-//
-//                if (!$chunk == "." || !$chunk == "..") {
-//
-//                    $this->attach_files($path."/".$chunk, $type);
-//
-//                }
-//
-//            }
-//
-//        }
-//
-//    }
-//
-//
-//
-//    /*
-//     * Публичная универсальная функция, для подключения файлов с кодом.
-//     */
-//    public function attach($path, $type)
-//    {
-//
-//        if (is_file($path)) {
-//
-//            $this->attach_file($path);
-//
-//        } elseif (is_dir($path)) {
-//
-//            $this->attach_files($path, $type);
-//
-//        } else {
-//            echo "Не удалось определить тип поключаемого файла или дириектории.";
-//        }
-//
-//    }
-//
-//}
-//
-//$system = new system();
-//$system->attach("v/stable/libraries/my", "all");
-//
-//
-
-
-
-
-
-
-//    if ($all) {
-//
-//        while ($filename = readdir(opendir($path)))
-//
-//
-//        $dir = "наш каталог";
-//        $catalog = opendir($dir);
-//
-//        while ($filename = readdir($catalog )) // перебираем наш каталог
-//        {
-//            $filename = $dir."/".$filename;
-//            include_once($filename); // один раз подрубаем, чтоб не повторяться
-//        }
-//
-//        closedir($catalog);
-//    }
-//
-//
-//    // TODO Допилить функцию, что бы выводило корректно ошибку, если файл не может быть выполнен.
-//    if (is_readable($path)) {
-//        require_once $path;
-//    } else {
-//        echo "Ошибка открытия файла.";
-//    }
-
-
-
-
-
-
-//
-//if ($_POST || $_REQUEST) {
-//
-//    if ($_POST['method'] == 'api' || $_REQUEST['method'] == 'api') {
-//
-//        echo "Используется API method";
-//
-//        if (isset($_POST['api_version']) || isset($_REQUEST['api_version'])) {
-//
-//            echo "Вы используете АПИ версии которая есть.";
-//
-//        } else {
-//
-//            echo "Не указана версия АПИ, использую стабильную.";
-//
-//        }
-//
-//    } else {
-//
-//        echo "Не обнаружен метод запроса.";
-//
-//    }
-//
-//} else {
-//
-//    if (isset($_SESSION['user']) && isset($_SESSION['user']['version_site'])) {
-//
-//        attach('v/'.$_SESSION['user']['version_site'].'/core.php');
-//
-//    } else {
-//
-//        attach('v/stable/core.php');
-//
-//    }
-//
-//}
-//
-//function database ($msg) {
-//    echo $msg;
-//}
-
-//$system = new system();
-//$system->attach("v");
+</body>
+</html>
+<form method="post" action="">
+    <label>
+        <p>Метод: </p>
+        <input type="text" name="method">
+    </label>
+    <label>
+        <p>Значение: </p>
+        <input type="text" name="value">
+    </label>
+    <input type="submit" value="GO">
+</form>
