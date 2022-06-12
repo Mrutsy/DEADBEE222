3643a102675c4cc11ea22f8d71b5b0597480c4ad
Mrutsy <bins@zhilk.in>
2022-06-12 18:39:45 +0700

    12.06.2022 v:1 1. Бэкап

--- index.php
+++ index.php
@@ -1,129 +1,42 @@
 <?php
+/*
+ * DEADBEE - Данный проект был задуман, как легкий помошник в настройке и развертывании своего собственного сайта на
+ * модульной основе, с версиями для каждого клиента.
+ * Создан мною для меня и немного для Вас:)
+ */
+
+
 
 /*
  * ОТЛАДКА
- * --- Разкомментировать для включения отладки.
+ * --- Раскомментировать для включения отладки.
  */
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 echo "<pre>";
-
-
-class system
-{
-    /**
-     * @var mixed|string
-     */
-    protected mixed $dir;
-
-    public function __construct ($current_dir = __DIR__)
-    {
-        $this->dir = $current_dir;
-    }
-
-
-    /*
-     * Функция и ее производные по подключению файлов к проекту.
-     */
-
-    public function attach ($path, $type = "ALL"): void
-    {
-
-        //echo "<br> Запуск функции Attach со значениями:  path - '$path',   type - '$type'. <br>";
-
-        if (is_file($this->dir."/".$path)) {
-
-            //echo "В указанном пути ($path) был обнаружен файл, проверяю, разрешен ли его тип к подключению.<br>";
-
-            if ($type == "ALL") {
-
-                //echo "Так как type = $type, то подключаю данный файл. <br>";
-                require_once $this->dir."/".$path;
-
-            } else {
-
-                $expansion = new SplFileInfo($this->dir."/".$path);
-
-                if ($expansion->getExtension() == $type) {
-
-                    //echo "Так как, текущий тип $type разрешен, подключаю файл.<br>";
-                    require_once $this->dir."/".$path;
-
-                } else {
-
-                    //echo "Данный тип не подходит к разрешенному типу ($type), пропускаю файл.<br>";
-
-                }
-
-            }
-
-        } elseif (is_dir($this->dir."/".$path)) {
-
-            //echo "По указанному пути ($this->dir/$path) находится дириектория. Захожу в нее. <br>";
-
-            foreach ( array_diff( scandir( $this->dir."/".$path ), array('..', '.')) as $chunk) {
-
-                //echo "Отправляю аргументы в функцию ATTACH. path = $path. <br>";
-
-                if ($chunk == "composer") {
-                    $this->attach($path."/".$chunk."/vendor/autoload.php", "php");
-                } else {
-                    $this->attach($path."/".$chunk, $type);
-                }
-
-            }
-
-        } else {
-            echo "BAD";
-        }
-    }
-
-    public function route(): void
-    {
-        /*
-         * Проверяем, авторизован ли пользователь, если авторизован, то заменяем ему в сессии значения маршрутизации.
-         */
-
-        if (isset($_SESSION['user']['authKey'])) {
-            echo "Пользователь авторизован.<br>";
-        } else {
-            echo "Пользователь не авторизован.<br>";
-        }
-    }
-
-    public function appeal (): void
-    {
-
-    }
-
-
-    private function connect_database (): void
-    {
-        $this->attach("v/stable/modules/system/database/connect.php");
-    }
-
-    public function db ()
-    {
-        $this->connect_database();
-    }
-
-    public function message
-}
-
-
-// Проверить метод обращение.
+// ------------------------------------------------>
 
 
 
-
-
-
-//$sys = new system(__DIR__);
-//$sys->attach("v/stable/modules", "php");
-
-
+// Запускаем функцию начала сессии.
 session_start();
+//session_destroy();
+
+/*
+ * Подключаем функцию для подлкючения файлов.
+ */
+require_once __DIR__."/v/stable/modules/system/libraries/attach.php";
+
+
+
+
+
+$attach = new attach();
+$attach->attach("../composer/vendor/autoload.php");
+$attach->attach("../database/connect.php");
+
+
 
 /*
  * Обрабатываем событие, при котором к проекту обращаются с помощью запросов.
@@ -137,10 +50,13 @@ if ($_POST || $_REQUEST) {
     }
 
     if ($request['method'] == "api") {
-        echo "Метод: ".$request['method'].". Путь: ".$request['route'];
+
+        echo "Метод: ".$request['method'];
+
     } elseif ($request['method'] == "route") {
+
         echo "Метод: ".$request['method'].". Путь: ".$request['route'];
-        //$sys->route();
+
     } else {
 
         //array_push($_SESSION['user']['systemMessage'], "apple", "raspberry");
@@ -154,6 +70,22 @@ if ($_POST || $_REQUEST) {
 
 
 } else {
-    echo "hmmm";
-    //$sys->route();
+
+    echo "Сайт открыт в обычном режиме.<br>";
+
+    if (isset($_SESSION['user']['versionProject'])) {
+        echo "У пользователя есть версия.<br>";
+        echo $_SESSION['user']['versionProject'];
+    } else {
+        echo "У пользователя нет версии.<br>";
+
+        $projectInfo = $GLOBALS['db']->query("SELECT * FROM settings", 123);
+        $projectInfo = $projectInfo->fetchAssoc();
+        var_dump($projectInfo);
+
+        $_SESSION['user']['versionProject'] = $projectInfo['projectVersionDefault'];
+        var_dump($_SESSION['user']);
+    }
+
+
 }
