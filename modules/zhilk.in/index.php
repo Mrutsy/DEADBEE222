7601b3f13a259c975d26ef29e2cf0f14cd4ba7dc
Mrutsy <bins@zhilk.in>
2022-06-13 01:00:04 +0700

    12.06.2022 v:4 1. Автоматическое изменение пути для ресурсов. Учитывая особенность, что в строке ссылки, не будет указан путь и то что файлы подключаются а не осуществяется переход в каждый файл.

--- modules/zhilk.in/index.php
+++ modules/zhilk.in/index.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Жилкин Роман</title>
     <meta name="description" content="Мой личный полигон для моих захватывающих идей 😈">
-    <link rel="icon" type="image/png" sizes="500x500" href="assets/img/zh(4).png">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="icon" type="image/png" sizes="500x500" href="<?php echo 'modules/'.basename(__DIR__); ?>/assets/img/zh(4).png">
+    <link rel="stylesheet" href="<?php echo 'modules/'.basename(__DIR__); ?>/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo 'modules/'.basename(__DIR__); ?>/assets/css/styles.css">
 </head>
 
 <body>
     <h1>Модуль загружен.</h1>
-    <h1>Test</h1>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <h1>Test</h1><img src="<?php echo 'modules/'.basename(__DIR__); ?>/assets/img/zh.png">
+    <script src="<?php echo 'modules/'.basename(__DIR__); ?>/assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- modules/zhilk.in/libraries/transform.php
+++ modules/zhilk.in/libraries/transform.php
@@ -9,6 +9,12 @@ class transform
 
             if (substr(strrchr($chunk, '.'), 1) == $find_type) {
 
+                //echo 'modules/'.basename(__DIR__)."/assets/";
+                //echo $path.$chunk."<br>";
+                $file_contents = file_get_contents($path.$chunk);
+                $file_contents = str_replace("assets/", "<?php echo 'modules/'.basename(__DIR__); ?>/assets/", $file_contents);
+                file_put_contents($path.$chunk, $file_contents);
+
                 rename("$path/$chunk", "$path/".basename($chunk, ".$find_type").".$result_type");
 
             }
@@ -19,3 +25,5 @@ class transform
 
 $start = new transform();
 $start->start(__DIR__."/../", "html", "php");
+
+//echo "modules/" . basename(__DIR__);
